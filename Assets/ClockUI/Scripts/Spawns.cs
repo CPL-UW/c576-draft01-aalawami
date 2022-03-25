70340700b22e44274402a6485dc088c5a8e26f1d
aalawami <71799489+aalawami@users.noreply.github.com>
2022-03-25 08:55:20 -0500

    commit

---
+++ Assets/ClockUI/Scripts/Spawns.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawns : MonoBehaviour
+{
+
+    public GameObject enemy;
+    float randX;
+    float randY;
+    Vector3 whereToSpawn;
+    public float spawnRate = 10f;
+    float nextSpawn = 0;
+    float enemNum = 0;
+    int randTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# c576-draft01-aalawami
+c576-draft01-aalawami created by GitHub Classroom
+
+
+It has a functioning clock with add and subtract button and spawns an enimy.
+However, it can't kill the enemy yet. I need help with that.
